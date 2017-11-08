4585e7bc28ac1d72217f9981319d1f1974b6376f
Maxim <32304530+xaubug@users.noreply.github.com>
2017-11-08 21:51:18 +0300

    Update HornersMeth.c

--- HornersMeth.c
+++ HornersMeth.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[]){
+    double polynom=0;
+    double x=0;
+    double n;
+    int i=0;
+    while (scanf("%lf",&n) == 1)
+    {
+        if(i == 0) x = n;
+        else polynom = polynom * x + n;
+        i++;
+    }
+    printf("%f\n", polynom);
+}
